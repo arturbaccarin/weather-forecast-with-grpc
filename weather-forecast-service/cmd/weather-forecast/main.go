aa85dda218d62bd50476587b785eb3330747c251
arturbaccarin <baccarin.artur@gmail.com>
2024-03-10 11:01:55 -0300

    feat: :construction: add weather api package

--- weather-forecast-service/cmd/weather-forecast/main.go
+++ weather-forecast-service/cmd/weather-forecast/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/arturbaccarin/weather-forecast-with-grpc/weather-forecast-service/config"
+	weatherapi "github.com/arturbaccarin/weather-forecast-with-grpc/weather-forecast-service/internal/weather_api"
+)
+
 func main() {
 	println("start project")
+
+	config := config.Load()
+
+	weatherAPI := weatherapi.NewWeatherAPI(config.WeatherAPIKey)
+
+	weatherAPI.SearchForCity("London")
+
 }
---
+++ weather-forecast-service/internal/weather_api/weather_api.go
@@ -0,0 +1,44 @@
+package weatherapi
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
+
+type WeatherAPI struct {
+	Key string
+}
+
+func NewWeatherAPI(key string) *WeatherAPI {
+	return &WeatherAPI{
+		Key: key,
+	}
+}
+
+func (w WeatherAPI) SearchForCity(city string) {
+	endpoint := "%s?key=%s&q=%s&aqi=no"
+
+	url := fmt.Sprintf(endpoint, WEATHER_API_URL, w.Key, city)
+	println(url)
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer response.Body.Close()
+
+	responseBody, _ := io.ReadAll(response.Body)
+
+	println(string(responseBody))
+}
